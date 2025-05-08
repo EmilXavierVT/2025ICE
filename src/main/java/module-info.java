ab4ad6d87af524f2c565887299309b703b5abd9e
EmilXavierVT <emilxavierthorsen@gmail.com>
2025-05-08 10:17:08 +0200

    initial commit 2025 8 may

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module dk.emilxaviervt._2025ice._2025ice {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens dk.emilxaviervt._2025ice._2025ice to javafx.fxml;
+    exports dk.emilxaviervt._2025ice._2025ice;
+}
