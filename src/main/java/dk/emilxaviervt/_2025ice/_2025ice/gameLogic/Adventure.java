33bc6f78c13f10b63e7eddaed7bc8b4c7a331ebd
EmilXavierVT <emilxavierthorsen@gmail.com>
2025-05-08 17:33:24 +0200

    secondary push

---
+++ src/main/java/dk/emilxaviervt/_2025ice/_2025ice/gameLogic/Adventure.java
@@ -0,0 +1,17 @@
+package dk.emilxaviervt._2025ice._2025ice.gameLogic;
+
+public class Adventure extends Thread{
+
+@Override
+public void run(){
+    boolean gameIsRunning = true;
+
+        while(gameIsRunning){
+
+
+        }
+
+
+}
+
+}
---
+++ src/main/java/dk/emilxaviervt/_2025ice/_2025ice/gameLogic/Main.java
@@ -0,0 +1,5 @@
+package dk.emilxaviervt._2025ice._2025ice.gameLogic;
+
+public class Main {
+    public static void main(String[] args) {new Adventure().start();}
+}
