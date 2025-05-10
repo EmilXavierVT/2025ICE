a76f5fbe6da4020fd3cd8dfb051cf2d7d1839c7e
EmilXavierVT <emilxavierthorsen@gmail.com>
2025-05-10 16:12:00 +0200

    refix of pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,5 +7,7 @@ module dk.emilxaviervt._2025ice {
 
     opens dk.emilxaviervt._2025ice to javafx.fxml;
     exports dk.emilxaviervt._2025ice.VFX;
+    exports dk.emilxaviervt._2025ice;
+    opens dk.emilxaviervt._2025ice.VFX to javafx.fxml;
 
 }
