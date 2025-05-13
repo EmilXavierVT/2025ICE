15b0b7cda36661274094ef7347ec21c2fc830a2b
luke-dkk <luke_persson@yahoo.dk>
2025-05-13 23:27:46 +0200

    Luke ActionPoint

--- src/main/java/dk/emilxaviervt/_2025ice/userLogic/ActionPoint.java
+++ src/main/java/dk/emilxaviervt/_2025ice/userLogic/ActionPoint.java
@@ -1,5 +1,26 @@
 package dk.emilxaviervt._2025ice.userLogic;
 
+import java.sql.Connection;
+
 public class ActionPoint {
+    private int ID;
+    private String description;
+    private String containItem;
+    private boolean itemNeeded;
+    private int goldCoins;
+    private int availableActionPoints;
+    private int containedCreature;
+    private boolean isFinal;
+    private boolean winnerActionPoint;
+    private boolean luckRoll;
+    private int changeAttackPoints;
+    private int changeHealthPoints;
+
+    Connection con;
+
+
+    public ActionPoint(){
+
+    }
 
 }
